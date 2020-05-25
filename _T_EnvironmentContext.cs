25597007a7aa95899647e182f49fcce9bf50d07b
Jaycee-Speedbug <speedbug@hotmail.fr>
2020-05-25 15:00:23 +0200

    Reset with NUnit

--- _T_EnvironmentContext.cs
+++ _T_EnvironmentContext.cs
@@ -1,22 +1,23 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using AeroCalcCore;
 
 
 namespace TestAeroCalc
 {
-    [TestClass]
+    [TestFixture]
     public class _T_EnvironmentContext
     {
-
-        public readonly EnvironmentContext EC;
-
-
-        [TestMethod]
-        public void _T_EnvironmentContext()
+        [SetUp]
+        public void Setup()
         {
-            EC = new EnvironmentContext("nothing.xml");
-            Assert.IsFalse(EC.configFilePath, "");
+            EnvironementContext EC = new EnvironementContext("nothing.xml");
+        }
 
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
         }
     }
 }
