ef05c45465c133de948c2e14a7cec3c945d50113
Jaycee-Speedbug <speedbug@hotmail.fr>
2020-05-29 19:56:06 +0200

    Configuration and Units loading started, to be debugged

--- _T_UnitsFile.cs
+++ _T_UnitsFile.cs
@@ -9,9 +9,9 @@ using System.Collections.Generic;
 namespace TestAeroCalc
 {
 
-
+    /// Test de fonctions de différents objets UnitsXMLFile et UnitsCSVFile
     [TestFixture]
-    public class _T_XMLFile
+    public class _T_UnitsFile
     {
 
         [SetUp]
@@ -24,13 +24,13 @@ namespace TestAeroCalc
 
 
         [Test]
-        public void _T_SaveUnits()
+        public void _T_LoadCSV_SaveXML()
         {
 
             // Création du dictionnaire
 
-            UnitsFile uf = new UnitsFile();
-            Units ud = uf.readFile(AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + "UnitsDictionary.csv");
+            UnitsCSVFile uf = new UnitsCSVFile();
+            Units ud = uf.getUnitsFromCSV(AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + "UnitsDictionary.csv");
             List<Unit> lu = ud.getUnits();
             Assert.IsTrue(lu.Count > 0);
             foreach (Unit u in lu)
@@ -38,7 +38,8 @@ namespace TestAeroCalc
                 System.Console.WriteLine(u.ToString());
             }
 
-            XMLFile xmlFile = new XMLFile();
+            //XMLFile xmlFile = new XMLFile();
+            UnitsXMLFile xmlFile = new UnitsXMLFile("");
             //xmlFile.setOutputFileAbsolutePath(AppDomain.CurrentDomain.BaseDirectory + "TEST-Units.xml");
             xmlFile.saveUnitDictionaryToXML(ud, AppDomain.CurrentDomain.BaseDirectory + "TEST-Units.xml");
 
---
+++ _T_UnitsXMLFile.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using AeroCalcCore;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace TestAeroCalc
+{
+
+
+    [TestFixture]
+    public class _T_UnitsXMLFile
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+
+
+
+        [Test]
+        public void _T_getUnitsFromXML()
+        {
+
+            // Création du dictionnaire
+
+            UnitsXMLFile uf = new UnitsXMLFile(AppDomain.CurrentDomain.BaseDirectory + 
+                                               Path.DirectorySeparatorChar + 
+                                               "Test_UnitsDictionary.xml");
+            Units ud = uf.getUnitsFromXML(AppDomain.CurrentDomain.BaseDirectory + 
+                                          Path.DirectorySeparatorChar + 
+                                          "Test_UnitsDictionary.xml");
+            Assert.IsNotNull(ud);
+
+            List<Unit> lu = ud.getUnits();
+            Assert.IsTrue(lu.Count > 1);
+            
+            foreach (Unit u in lu)
+            {
+                System.Console.WriteLine(u.ToString());
+            }
+
+            /*
+            // Enregistrement du dictionnaire
+            UnitsXMLFile xmlFile = new UnitsXMLFile("");
+            xmlFile.saveUnitDictionaryToXML(ud, AppDomain.CurrentDomain.BaseDirectory + "TEST-Units.xml");
+            */
+
+
+
+        }
+
+    }
+
+}
